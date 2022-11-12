0fe127d9a740e48e9613ee4ba89bc1b71ca021b9
SGman98 <bastiangarnica@gmail.com>
2022-11-12 16:59:29 -0500

    Add initial scripts for databases

---
+++ sa_bank_ss/init.sql
@@ -0,0 +1,27 @@
+INSERT INTO accounts (
+        id,
+        firstname,
+        lastname,
+        balance,
+        iduser,
+        created_at,
+        updated_at
+    )
+VALUES (
+        1,
+        'Jeisson',
+        'Vergara',
+        99999.0,
+        1,
+        '1999-01-08 04:05:06',
+        '1999-01-08 04:05:06'
+    ),
+    (
+        2,
+        'Dolly',
+        'Montoya',
+        99999.0,
+        2,
+        '1999-01-08 04:05:06',
+        '1999-01-08 04:05:06'
+    );
---
+++ sa_user_ss/init.sql
@@ -0,0 +1,27 @@
+INSERT INTO users (
+        id,
+        firstname,
+        lastname,
+        email,
+        account,
+        created_at,
+        updated_at
+    )
+VALUES (
+        1,
+        'Jeisson',
+        'Vergara',
+        'jeissonvergara@unal.edu.co',
+        1,
+        '1999-01-08 04:05:06',
+        '1999-01-08 04:05:06'
+    ),
+    (
+        2,
+        'Dolly',
+        'Montoya',
+        'dollymontoya@unal.edu.co',
+        2,
+        '1999-01-08 04:05:06',
+        '1999-01-08 04:05:06'
+    );
